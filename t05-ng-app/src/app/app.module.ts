bd6884b1132656a6a9040fa8f20e9bb2ccb5c03d
levu1 <45563152+Vukeezy@users.noreply.github.com>
2020-04-08 14:21:56 +0200

    dodat nav_menu i zapocet frontend za dodavanje lekara

--- t05-ng-app/src/app/app.module.ts
+++ t05-ng-app/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddDoctorFormComponent } from './add-doctor-form/add-doctor-form.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    AddDoctorFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
