343fe069f06298bf9b9c21989823ffd244b1b602
levu1 <45563152+Vukeezy@users.noreply.github.com>
2020-04-28 22:12:37 +0200

    zavrsen backend deo za dodavanje tipa pregleda

---
+++ tim05/tim05/src/main/java/com/project/tim05/api/AppointmentTypeController.java
@@ -0,0 +1,45 @@
+package com.project.tim05.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.tim05.dto.AppointmentTypeDTO;
+import com.project.tim05.model.AppointmentType;
+import com.project.tim05.service.AppointmentTypeService;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/appointmentType")
+@RestController
+public class AppointmentTypeController {
+
+	private final AppointmentTypeService ats;
+
+	@Autowired
+	public AppointmentTypeController(AppointmentTypeService ats) {
+		super();
+		this.ats = ats;
+	}
+	
+	@PostMapping("/addAppointmentType")
+	public void addAppointmentType(@RequestBody AppointmentTypeDTO atDTO) {
+		AppointmentType at = new AppointmentType(atDTO.getName());
+		
+		ats.addAppointmentType(at);
+		
+	}
+	
+	@GetMapping("/getAppointmentTypes")
+	public List<AppointmentType> getAppointmetTypes(){
+		return ats.getAppointmentTypes();
+	}
+	
+	
+	
+}
