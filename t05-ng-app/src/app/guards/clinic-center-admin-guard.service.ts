86e9a315da68e3af819917a800e15a24ae854495
romana98 <romana_1998@hotmail.com>
2020-05-05 01:49:57 +0200

    resavanje konflikta

--- t05-ng-app/src/app/guards/clinic-center-admin-guard.service.ts
+++ t05-ng-app/src/app/guards/clinic-center-admin-guard.service.ts
@@ -10,18 +10,8 @@ export class ClinicCenterAdminGuardService {
   constructor(private router: Router,private authservice: AuthenticationService) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 
-<<<<<<< HEAD
-    let bar : String = String(sessionStorage.getItem('role'));
-    let str : String = String("ROLE_CLINIC_CENTER_ADMIN");
 
-    console.log(sessionStorage.getItem('role').localeCompare(str.toString()));
-    console.log(bar === str);
-
-    console.log(this.authservice.isUserLoggedIn());
-    if(sessionStorage.getItem('role').localeCompare("ROLE_CLINIC_CENTER_ADMIN") === 0){
-=======
     if(sessionStorage.getItem('role')===("ROLE_CLINIC_CENTER_ADMIN")){
->>>>>>> refs/remotes/origin/master
       return true;
     }
     else{
