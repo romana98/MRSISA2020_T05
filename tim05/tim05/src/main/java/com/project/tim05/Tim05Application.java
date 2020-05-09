380dfcd148139ee56baabd1fb4d68be66f08fd6e
romana98 <romana_1998@hotmail.com>
2020-05-10 00:46:16 +0200

    devops proba

--- tim05/tim05/src/main/java/com/project/tim05/Tim05Application.java
+++ tim05/tim05/src/main/java/com/project/tim05/Tim05Application.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Tim05Application {
 
 	public static void main(String[] args) {
-		System.setProperty("java.runtime.version", "1.8");
 		SpringApplication.run(Tim05Application.class, args);
 	}
 
