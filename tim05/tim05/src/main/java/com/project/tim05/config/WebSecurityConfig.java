d3b8f56a77e0d0b13b1ca60c46ea8a4511cd3070
romana98 <romana_1998@hotmail.com>
2020-05-13 01:38:27 +0200

    reg popravka

--- tim05/tim05/src/main/java/com/project/tim05/config/WebSecurityConfig.java
+++ tim05/tim05/src/main/java/com/project/tim05/config/WebSecurityConfig.java
@@ -80,8 +80,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		// TokenAuthenticationFilter ce ignorisati sve ispod navedene putanje
-		web.ignoring().antMatchers(HttpMethod.POST, "/auth/login");
-		web.ignoring().antMatchers(HttpMethod.POST, "/registrationRequests/register");
+		web.ignoring().antMatchers(HttpMethod.POST, "/auth/login", "/registrationRequests/register");
 		web.ignoring().antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "/favicon.ico", "/**/*.html",
 				"/**/*.css", "/**/*.js");
 		
