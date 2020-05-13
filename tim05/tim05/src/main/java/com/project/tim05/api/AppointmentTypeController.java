f109ab240519f2f6723c8e7eaea3e2f9827cda1d
miloradradovic <45543741+miloradradovic@users.noreply.github.com>
2020-05-14 00:32:05 +0200

    Brisanje tipa pregleda, back deo

--- tim05/tim05/src/main/java/com/project/tim05/api/AppointmentTypeController.java
+++ tim05/tim05/src/main/java/com/project/tim05/api/AppointmentTypeController.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.tim05.dto.AppointmentTypeDTO;
@@ -23,7 +25,7 @@ import com.project.tim05.service.ClinicAdministratorService;
 @CrossOrigin(origins = "https://eclinic05.herokuapp.com")
 @RequestMapping("/appointmentType")
 @RestController
-public class AppointmentTypeController {
+public class AppointmentTypeController<T> {
 
 	private final AppointmentTypeService ats;
 	private final ClinicAdministratorService cas;
@@ -56,6 +58,22 @@ public class AppointmentTypeController {
 		return ResponseEntity.ok(ats.getAppointmentTypes());
 	}
 	
+	@DeleteMapping("/deleteAppointmentType")
+	@PreAuthorize("hasRole('CLINIC_ADMIN')")
+	public ResponseEntity<T> deleteAppointmentType(@RequestParam String aptype_id){
+		int flag = ats.deleteAppointmentType(Integer.parseInt(aptype_id));
+		if(flag != 0) {
+			return ResponseEntity.status(HttpStatus.OK).body(null);
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(null);
+		}
+	}
 	
-	
+	@GetMapping("/getClinicAppointmenTypes")
+	@PreAuthorize("hasRole('CLINIC_ADMIN')")
+	public ResponseEntity<List<AppointmentType>> getClinicAppointmentTypes(@RequestParam String clinic_id){
+		return ResponseEntity.ok(ats.getClinicAppointmentTypes(Integer.parseInt(clinic_id)));
+	}
 }
+
